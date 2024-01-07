21b833cc00ae32e71422479d652f1700ef18d0b8
wa-yuri77 <yuri77wada447@gmail.com>
2024-01-07 14:48:22 +0900

    built application

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "aws17app14": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
